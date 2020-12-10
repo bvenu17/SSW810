d3a2a19274081c11a86af2d298bc60cab27f9d5a
Venugopal Balaji <37976236+bvenu17@users.noreply.github.com>
2020-12-09 21:15:32 -0500

    feat: html templates for flask

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
-
+import sqlite3
+from Student_Repository_Venugopal_Balaji import University
 app: Flask = Flask(__name__)
 
-@app.route('/test')
-def test() -> str:
-    return "Testing server"
+@app.route('/')
+def home() -> str:
+    return render_template('base.html',title='Student Repository')
+
+@app.route('/students')
+def student_grades():
+    query: str = """select s.Name,s.CWID,g.Course,g.Grade,i.Name from students as s join grades as g join instructors as i where s.CWID=g.StudentCWID and g.InstructorCWID=i.CWID order by s.Name;"""
+    uni: University = University(r"Files")
+    # print(uni.student_grades_details_db("Student_Repository_Venugopal_Balaji.sqlite"))
+    details = uni.student_grades_details_db("Student_Repository_Venugopal_Balaji.sqlite")
+
+    return render_template('student_grades.html', title='Student Repository - Grades',
+                           students=details)
 
 app.run(debug=True)
